522677806f83e18e39cd1111c22966738e9d7427
Flora-King <giflona@yahoo.co.uk>
2023-01-23 20:37:54 +0000

    Update js code for quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,179 +1,188 @@
 
-const questions = [
+var questionsData = [
     {
-        question: "How many players are in a cricket team?",
-        a: "8",
-        b: "9",
-        c: "10",
-        d: "11",
-        correct: "d",
+    question: 'How many players are in a cricket team?',
+    a:'8',
+    b:'9',
+    c:'10',
+    d:'11',
+    correct:"d",
     },
     {
-        question: "Who was the 40th President of the United States?",
-        a: "Franklin D. Roosevelt",
-        b: "Ronald Reagan",
-        c: "Bill Clinton",
-        d: "George W. Bush",
-        correct: "a",
+    question: "Who was the 40th President of the United States?",
+    a:"Franklin D. Roosevelt",
+    b:"Ronald Reagan",
+    c:"Bill Clinton",
+    d:"George W. Bush",
+    correct:"a",
     },
     {
-        question: "What is the name of the dog in Tintin?",
-        a: "Snowy",
-        b: "Flakes",
-        c: "Dottie",
-        d: "Luna",
-        correct: "a",
+    question: "What is the name of the dog in Tintin?",
+    a:"Snowy",
+    b: "Flakes",
+    c: "Dottie",
+    d: "Luna",
+    correct: "a",
     },
     {
-        question: "How many sides has a Hexagon?",
-        a: "5",
-        b: "6",
-        c: "7",
-        d: "8",
-        correct: "b",
+    question: "How many sides has a Hexagon?",
+    a: "5",
+    b: "6",
+    c: "7",
+    d: "8",
+    correct: "b",
     },
     {
-        question: "When did Salt Lake City host the Winter Olympics?",
-        a: "1992",
-        b: "1998",
-        c: "2002",
-        d: "2008",
-        correct: "c",
+    question: "When did Salt Lake City host the Winter Olympics?",
+    a: "1992",
+    b: "1998",
+    c: "2002",
+    d: "2008",
+    correct: "c",
     },
     {
-        question: "Who is the CEO of Amazon?",
-        a: "Elon Musk",
-        b: "Tim Cook",
-        c: "Mark Zuckerberg",
-        d: "Andy Jessy",
-        correct: "d",
+    question: "Who is the CEO of Amazon?",
+    a: "Elon Musk",
+    b: "Tim Cook",
+    c: "Mark Zuckerberg",
+    d: "Andy Jessy",
+    correct: "d",
     },
     {
-        question: "In The Lion King, who is Simba’s uncle?",
-        a: "Mufasa",
-        b: "Scar",
-        c: "Timon",
-        d: "Zazu",
-        correct: "b",
+    question: "In The Lion King, who is Simba’s uncle?",
+    a: "Mufasa",
+    b: "Scar",
+    c: "Timon",
+    d: "Zazu",
+    correct: "b",
     },
     {
-        question: "Which one of the following is the correct spelling?",
-        a: "Mantenance",
-        b: "Maintenance",
-        c: "Miantenance",
-        d: "Maintenence",
-        correct: "b",
+    question: "Which one of the following is the correct spelling?",
+    a: "Mantenance",
+    b: "Maintenance",
+    c: "Miantenance",
+    d: "Maintenence",
+    correct: "b",
     },
     {
-        question: "How many elements are there on the periodic table?",
-        a: "58",
-        b: "78",
-        c: "98",
-        d: "118",
-        correct: "d",
+    question: "How many elements are there on the periodic table?",
+    a: "58",
+    b: "78",
+    c: "98",
+    d: "118",
+    correct: "d",
     },
     {
-        question: "If you are born on the 1st of January, which star sign are you?",
-        a: "Scorpio",
-        b: "Carpricorn",
-        c: "Libra",
-        d: "Aries",
-        correct: "b",
+    question: "If you are born on the 1st of January, which star sign are you?",
+    a: "Scorpio",
+    b: "Carpricorn",
+    c: "Libra",
+    d: "Aries",
+    correct: "b",
     },
     {
-        question: "What does the Richter scale measure?",
-        a: "Wind Speed",
-        b: "Temperature",
-        c: "Tornado Strength",
-        d: "Earthquake intensity",
-        correct: "d",
+    question: "What does the Richter scale measure?",
+    a: "Wind Speed",
+    b: "Temperature",
+    c: "Tornado Strength",
+    d: "Earthquake intensity",
+    correct: "d",
     },
     {
-        question: "What’s Garfield favourite food?",
-        a: "Pizza",
-        b: "Lasagna",
-        c: "Burger",
-        d: "Sandwich",
-        correct: "b",
+    question: "What’s Garfield favourite food?",
+    a: "Pizza",
+    b: "Lasagna",
+    c: "Burger",
+    d: "Sandwich",
+    correct: "b",
     },
     {
-        question: "Which chemical element has Ag as a symbol?",
-        a: "Gold",
-        b: "Silver",
-        c: "Iron",
-        d: "Carbon",
-        correct: "b",
+    question: "Which chemical element has Ag as a symbol?",
+    a: "Gold",
+    b: "Silver",
+    c: "Iron",
+    d: "Carbon",
+    correct: "b",
     },
     {
-        question: "Who painted ‘Girl with a Pearl Earring’?",
-        a: "Johannes Vermeer",
-        b: "Vincent Van Gogh",
-        c: "Leonardo da Vinci",
-        d: "Sandro Botticelli",
-        correct: "a",
+    question: "Who painted ‘Girl with a Pearl Earring’?",
+    a: "Johannes Vermeer",
+    b: "Vincent Van Gogh",
+    c: "Leonardo da Vinci",
+    d: "Sandro Botticelli",
+    correct: "a",
     },
     {
-        question: "Which Nobel Prize did Winston Churchill win?",
-        a: "Literature",
-        b: "Peace",
-        c: "Chemistry",
-        d: "Physics",
-        correct: "a",
+    question: "Which Nobel Prize did Winston Churchill win?",
+    a: "Literature",
+    b: "Peace",
+    c: "Chemistry",
+    d: "Physics",
+    correct: "a",
     },
 ];
 
-const quiz = document.getElementById('quiz');
-const answer = document.querySelectorAll('.answer');
-const question = document.getElementById('question-text');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+var quiz = document.getElementById("quiz");
+var answerEls = document.querySelectorAll(".answer");
+var questionEl = document.getElementById("question-text");
+var a_text = document.getElementById("a_text");
+var b_text = document.getElementById("b_text");
+var c_text = document.getElementById("c_text");
+var d_text = document.getElementById("d_text");
+var submitBtn = document.getElementById("submit");
 
+let currentQuiz = 0;
+let score = 0;
 
+loadQuiz();
 
+function loadQuiz() {
+    deselectAnswers();
 
-/**
- * let score = 0;
-let currentQuestion = 0;
+    var currentQuizData = questionsData[currentQuiz];
 
-function startQuiz() {
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 
 }
 
-function deselectAnswers () {
+function deselectAnswers() {
+answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
 
-}
+});
+};
 
 function getSelected() {
 
+let answer;
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
 }
 
-function runGame() {
+submitBtn.addEventListener('click', () => {
+    var answer = getSelected();
 
-}
+    if(answer) {
+        if(answer === questionsData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
 
-function displayQuestion() {
-    
-}
-
-function checkAnswer() {
-
-}
-
-function incrementCorrectAnswer() {
-
-}
-
-function incrementWrongAnswer() {
-    
-}
-
-function incrementAttempted() {
-    
-}
-
-function displayNextQuestion() {
-    
-}*/
+        if(currentQuiz < questionsData.length) {
+        loadQuiz();
+        } else {
+            quiz.innerHTML =`
+            <h2> You answered $(score)/${questionsData.length} questions correctly.</h2>
+            <button onclick="location.reload()">Reload</button>
+            `;
+             }
+        }
+});
